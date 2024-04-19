065cf70406f96816bdd486762d9955277ea67e14
nikhil2119 <nikhildhiman2119@gmail.com>
2024-04-19 23:01:02 +0530

    push

--- Header.jsx
+++ Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-        <h1 className='title'>Notes</h1>
+        <h1 className='title'>Note</h1>
     </div>
   )
 }
